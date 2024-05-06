57c5b37043849647d134d61207eb37fef143eefe
Tanaes <jonsan@gmail.com>
2024-05-06 01:35:17 -0700

    fixes

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -5,18 +5,18 @@ import re
 
 regexes = {
     "nf-reads-profiler": ["v_pipeline.txt", r"(\S+)"],
+    "FASTP": ["v_fastp.txt", r"(\S+)"],
     "Nextflow": ["v_nextflow.txt", r"(\S+)"],
     "MetaPhlAn": ["v_metaphlan.txt", r"MetaPhlAn version (\S+)"],
     "HUMAnN": ["v_humann.txt", r"humann v(\S+)"],
-    "qiime": ["v_qiime.txt", r"(\S+)"],
     "MultiQC": ["v_multiqc.txt", r"(\S+)"],
 }
 results = OrderedDict()
 results["nf-reads-profiler"] = '<span style="color:#999999;">N/A</span>'
+results["FASTP"] = '<span style="color:#999999;">N/A</span>'
 results["Nextflow"] = '<span style="color:#999999;">N/A</span>'
 results["MetaPhlAn"] = '<span style="color:#999999;">N/A</span>'
 results["HUMAnN"] = '<span style="color:#999999;">N/A</span>'
-results["qiime"] = '<span style="color:#999999;">N/A</span>'
 results["MultiQC"] = '<span style="color:#999999;">N/A</span>'
 
 # Search each file using its regex
